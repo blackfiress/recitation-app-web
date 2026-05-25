b3160f6ef1e374b12137a8b17429fe4180523dba
blackfiress <39981193+blackfiress@users.noreply.github.com>
2026-05-25 18:15:27 +0800

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+export default defineConfig({
+  root: "client",
+  plugins: [react()],
+  build: { outDir: path.resolve("client", "dist") },
+  server: { proxy: { "/api": "http://localhost:3001" } }
+});
