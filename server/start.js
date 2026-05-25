e9242f44071e3c532586deccb489333727aaa354
blackfiress <39981193+blackfiress@users.noreply.github.com>
2026-05-25 18:15:54 +0800

    Add server/start.js

---
+++ server/start.js
@@ -0,0 +1,17 @@
+﻿import { spawn } from "child_process";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const serverPath = path.join(__dirname, "index.js");
+
+const child = spawn("node", [serverPath], {
+  stdio: "inherit",
+  detached: false,
+});
+
+process.on("SIGINT", () => { child.kill(); process.exit(); });
+process.on("SIGTERM", () => { child.kill(); process.exit(); });
+
+// Keep alive with a simple HTTP keepalive
+setInterval(() => {}, 60000);
